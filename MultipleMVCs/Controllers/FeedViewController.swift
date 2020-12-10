211b739444c8603762f103940bb2ec84a7018d79
Tung Vu Duc <tungvuduc2805@gmail.com>
2020-12-10 10:47:32 +0700

    add .gitignore

--- MultipleMVCs/Controllers/FeedViewController.swift
+++ MultipleMVCs/Controllers/FeedViewController.swift
@@ -7,21 +7,33 @@
 
 import UIKit
 
+class ImageCellController {
+    
+    private let item: FeedItem
+    private var cell: FeedImageCell?
+    
+    init(item: FeedItem) {
+        self.item = item
+    }
+    
+    func view(_ tableView: UITableView) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "FeedImageCell") as! FeedImageCell
+        cell.feedImageView.image = UIImage(named: item.image)
+        cell.usernameLabel.text = item.username
+        cell.dateLabel.text = item.date
+        cell.descriptionLabel.text = item.description
+        return cell
+    }
+    
+}
+
 class FeedViewController: UITableViewController {
     
-    var dataSource: [FeedItem] = [] {
+    var dataSource: [ImageCellController] = [] {
         didSet {
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
-        }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        APIService.shared.loadFeed {[weak self] feed in
-            self?.dataSource = feed
         }
     }
     
@@ -30,13 +42,8 @@ class FeedViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell =  tableView.dequeueReusableCell(withIdentifier: "FeedImageCell") as! FeedImageCell
-        let item = dataSource[indexPath.row]
-        cell.feedImageView.image = UIImage(named: item.image)
-        cell.usernameLabel.text = item.username
-        cell.dateLabel.text = item.date
-        cell.descriptionLabel.text = item.description
-        return cell
+        let cellController = dataSource[indexPath.row]
+        return cellController.view(tableView)
     }
     
 }
